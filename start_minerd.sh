941c35f6b6288787fd7384520dcf70890c6eb9ce
HailinGuo <hazirguo@gmail.com>
2013-12-10 14:18:06 +0800

    Update start_minerd.sh

--- start_minerd.sh
+++ start_minerd.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
-./minerd -B -a scrypt-jane -o http://pool.ybcoin.com:8337 -u tom.1 -p x -t 32
+./minerd -B -a scrypt-jane -o http://pool.ybcoin.com:8337 -u haifenglinying.guohl -p 123 -t 4
